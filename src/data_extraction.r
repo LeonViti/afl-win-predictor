34deb1473b83ee058d2b8bb60852a28ee7967f91
LeonViti <leon_viti@outlook.com>
2025-12-14 21:05:10 +1100

    add data to git ignore

--- src/data_extraction.r
+++ src/data_extraction.r
@@ -1,7 +1,13 @@
+# libraries 
 library(fitzRoy) # for loading of data
 library(arrow) # for storing data as a parquet
+
+# Loading of Data
 # get the individual player data
-player_stats = fetch_player_stats_afl(season = 2023)
+player_stats_2023 = fetch_player_stats_afl(season = 2023)
 
 # get the squiggle fixture data
 fixture_23 <- fetch_fixture_squiggle(2023)
+
+# save as Parquet
+write_
