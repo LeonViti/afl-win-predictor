651ab93fa023fb1b5f3bcbb9c7d87b09b10996f9
LeonViti <leon_viti@outlook.com>
2025-12-14 21:01:07 +1100

    updated lockfile to include the library arrow

--- src/data_extraction.r
+++ src/data_extraction.r
@@ -1,4 +1,7 @@
-library(fitzRoy)
+library(fitzRoy) # for loading of data
+library(arrow) # for storing data as a parquet
 # get the individual player data
 player_stats = fetch_player_stats_afl(season = 2023)
 
+# get the squiggle fixture data
+fixture_23 <- fetch_fixture_squiggle(2023)
