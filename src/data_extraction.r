7b0aa5272ae5901486ebf08bf9c0ef206104ab21
LeonViti <leon_viti@outlook.com>
2025-12-14 21:29:12 +1100

    added loop to extract fixture data

--- src/data_extraction.r
+++ src/data_extraction.r
@@ -3,11 +3,14 @@ library(fitzRoy) # for loading of data
 library(arrow) # for storing data as a parquet
 
 # Loading of Data
-# get the individual player data
-player_stats_2023 = fetch_player_stats_afl(season = 2023)
-
 # get the squiggle fixture data
-fixture_23 <- fetch_fixture_squiggle(2023)
+# NOTE: 1899 is the first date that contains data
 
-# save as Parquet
-write_
+# iterate over all seasons and save as a parquet
+for (i in 1899:2025) {
+    # Fetch fixture data for year i
+    fixture <- fetch_fixture_squiggle(i)
+
+    # Save to Parquet
+    write_parquet(fixture, paste0("data/squiggle_fixture_", i, ".parquet"))
+}
